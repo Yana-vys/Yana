fe11ec2123b74bf99efc6e718928d28cb40d81c2
Yana-vys <vysyana21@gmail.com>
2025-04-17 17:39:49 +0000

    fix: correct visit counter implementation

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-visit_count =0
+visit_count = 0
 
 @app.route('/')
 def index():
     global visit_count
     visit_count += 1
     return f"Всего посещений: {visit_count}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
